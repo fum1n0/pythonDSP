71efa0288b0f96a4458cb123bc01a94034a0213c
fum1n0 <g1521025@outlook.com>
2018-01-20 11:44:19 +0900

    command line option add

--- mix_wave.py
+++ mix_wave.py
@@ -1,29 +1,35 @@
 #coding:utf-8
+import argparse
 
 from signal_function import *
 from pylab import *
 
 if __name__ == "__main__":
 
-    args = sys.argv # 音声ファイルのパス
-    num = len(args) - 1
-
+    # 音声ファイルのパス取得
+    parser = argparse.ArgumentParser(description='mix wave')
+    parser.add_argument('-p','--path', nargs='*', help="mix wave file path",
+                        required=True)
+    args = parser.parse_args()
+    path = args.path
+    
+    num = len(args.path)
     wav_list = []
     fs_list = []
     length_list = [] 
 
     # 音声をロード
-    if(len(args)<=2):
-        print("no mix")
-        sys.exit()
-    else:
-        for i in range(1,len(args)):
-            wav, fs = wavread(args[i])
+    if 2 <= len(path):
+        for i in range(0,len(path)):
+            wav, fs = wavread(path[i])
             # 正規化
             wav = wav * (1.0/num) * np.amax(np.absolute(wav))
             length_list.append(len(wav))
             wav_list.append(wav.tolist())
             fs_list.append(fs)
+    else:
+        print("no mix")
+        sys.exit()
     
     # 0 padding
     max_length = max(length_list)
@@ -40,9 +46,4 @@ if __name__ == "__main__":
     print("len: " + str(len(signal)/min(fs_list)) + "[sec]")
     print("fs: " + str(min(fs_list)))
     writeWave(signal, min(fs_list), name="wav/mix")
-
-
-
-
-
-
+    print("mix finish")
