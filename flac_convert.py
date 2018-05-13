56cb16a153575b268e4780ab6a93dde1c52032fa
fum1n0 <g1521025@outlook.com>
2018-05-14 04:42:47 +0900

    add tag file extension, not fix mv

--- flac_convert.py
+++ flac_convert.py
@@ -13,58 +13,88 @@ from mutagen.mp4 import MP4
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--data_dir', dest='data_dir',
                     default='.', help='path of the music data')
+parser.add_argument('--tag_ex', dest='tag_ex',
+                    default='m4a', help='ectension of the tag file')
+parser.add_argument('--move', dest='move', type=bool, 
+                    default='False', help='file move dir')
+
 args = parser.parse_args()
 
 if __name__ == '__main__':
 
-    m4a_dir = '{}/m4a'.format(args.data_dir)
+    # file dir path
+    tag_dir = '{}/{}'.format(args.data_dir, args.tag_ex)
     wav_dir = '{}/wav'.format(args.data_dir)
     flac_dir = '{}/flac'.format(args.data_dir)
 
-    if not os.path.exists(m4a_dir):
-        print("Not m4a directory")
-        os.sys.exit()
+    # tag dir path check
+    if not os.path.exists(tag_dir):
+        os.mkdir(tag_dir)
 
+    if args.move:  # move tag file to tag dir
+        from_path = '{}/*.{}'.format(args.data_dir, args.tag_ex)
+        to_path = '{}/'.format(tag_dir)
+        cli_args = ['mv', from_path, to_path]
+        try:
+            subprocess.check_output(cli_args)
+        except:
+            print("not move " + args.tag_ex + " file")
+
+    # wav dir path check
     if not os.path.exists(wav_dir):
-        print("Not m4a directory")
-        os.sys.exit()
+        os.mkdir(wav_dir)
 
+    if args.move:  # move wav file to wav dir
+        from_path = '{}/*.wav'.format(args.data_dir)
+        to_path = '{}/'.format(wav_dir)
+        cli_args = ['mv', from_path, to_path]
+        try:
+            subprocess.check_output(cli_args)
+        except:
+            print('not move wav file')
+
+    # flac dir check
     if not os.path.exists(flac_dir):
         os.mkdir(flac_dir)
 
-    m4a_path = glob('{}/*.m4a'.format(m4a_dir))
+    # file path glob
+    tag_path = glob('{}/*.{}'.format(tag_dir, args.tag_ex))
     wav_path = glob('{}/*.wav'.format(wav_dir))
     flac_path = []
-
-    for path in m4a_path:
+    for path in tag_path:
         flac_path.append('{}/'.format(flac_dir) +
                          os.path.basename(path[:-4]) + '.flac')
 
-    if len(m4a_path) != len(wav_path):
-        print("no match m4a,wav")
+    if len(tag_path) != len(wav_path):
+        print('no match num {},wav'.format(args.tag_ex))
         os.sys.exit()
 
     print(wav_path)
     print(flac_path)
 
-    args = ['ffmpeg', '-i', m4a_path[0], 'cover.jpg']
+    # output cover image
+    cli_args = ['ffmpeg', '-i', tag_path[0], 'cover.jpg']
     try:
-        subprocess.check_output(args)
+        subprocess.check_output(cli_args)
     except:
         print("not output cover")
 
+    # convert
     for i in range(len(wav_path)):
-        args = ['ffmpeg', '-i', wav_path[i], '-ac',
-                '2', '-acodec', 'flac', flac_path[i]]
+
+        # convert wav file to flac
+        cli_args = ['ffmpeg', '-i', wav_path[i], '-ac',
+                    '2', '-acodec', 'flac', flac_path[i]]
         try:
-            subprocess.check_output(args)
+            subprocess.check_output(cli_args)
         except:
             print("wav to flac error")
 
-        args = ['ffmpeg', '-i', m4a_path[i], '-ac',
-                '2', '-acodec', 'flac', 'tag.flac']
+        # convert tag file to flac
+        cli_args = ['ffmpeg', '-i', tag_path[i], '-ac',
+                    '2', '-acodec', 'flac', 'tag.flac']
         try:
-            subprocess.check_output(args)
+            subprocess.check_output(cli_args)
         except:
             print("convert tag flac error")
 
@@ -90,17 +120,17 @@ if __name__ == '__main__':
         # save music info
         audio_flac.save()
 
-        # remove cover image
-        args = ['rm', './tag.flac']
+        # remove flac tag file
+        cli_args = ['rm', './tag.flac']
         try:
-            subprocess.check_output(args)
+            subprocess.check_output(cli_args)
         except:
             print("remove tag flac error")
 
     # remove cover image
-    args = ['rm', './cover.jpg']
+    cli_args = ['rm', './cover.jpg']
     try:
-        subprocess.check_output(args)
+        subprocess.check_output(cli_args)
     except:
         print("remove cover image error")
 
